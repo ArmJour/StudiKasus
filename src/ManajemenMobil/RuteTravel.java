8131be7be327948063e86cea2d59e489ea937e32
RozdanHanyv <164728376+RozdanHanyv@users.noreply.github.com>
2024-11-26 07:36:06 +0700

    Add files via upload

---
+++ src/ManajemenMobil/RuteTravel.java
@@ -0,0 +1,30 @@
+package ManajemenMobil;
+
+class RuteTravel {
+    private String asal;
+    private String tujuan;
+    private int biaya;
+
+    // Konstruktor
+    public RuteTravel(String asal, String tujuan, int biaya) {
+        this.asal = asal;
+        this.tujuan = tujuan;
+        this.biaya = biaya;
+    }
+
+    public String getAsal() {
+        return asal;
+    }
+
+    public String getTujuan() {
+        return tujuan;
+    }
+
+    public int getBiaya() {
+        return biaya;
+    }
+
+    public void tampilkanRute(int nomor) {
+        System.out.printf("%d. %-15s %-10s Rp%-10d\n", nomor, asal, tujuan, biaya);
+    }
+}
